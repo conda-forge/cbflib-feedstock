ab67484bdc1b34f5634bde50800184eadc59608b
Johan Hattne <hattne@users.noreply.github.com>
2025-07-06 11:00:13 -0700

    Refresh patches from current CBFlib (#6)
    * Refresh patches from current CBFlib
    Update dependencies, bump build number.
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.50.1, and conda-forge-pinning 2025.07.04.13.43.03

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,14 +6,15 @@ else
     data_input="${SRC_DIR}/data-input/CBFlib_0.9.7_Data_Files_Input"
 fi
 
-cmake ${CMAKE_ARGS}                                 \
-    -DCBF_DATA_INPUT:PATH="${data_input}"           \
-    -DCBF_DATA_OUTPUT:PATH="${SRC_DIR}/data-output" \
-    -DCBF_ENABLE_FORTRAN:BOOL=OFF                   \
-    -DCBF_ENABLE_JAVA:BOOL=OFF                      \
-    -DCBF_ENABLE_PYTHON:BOOL=OFF                    \
-    -DCBF_WITH_HDF5:BOOL=OFF                        \
-    -DCBF_WITH_LIBTIFF:BOOL=OFF                     \
+cmake ${CMAKE_ARGS}                                      \
+    -DCBF_WITH_DATA_INPUT:PATH="${data_input}"           \
+    -DCBF_WITH_DATA_OUTPUT:PATH="${SRC_DIR}/data-output" \
+    -DCBF_ENABLE_FORTRAN:BOOL=OFF                        \
+    -DCBF_ENABLE_JAVA:BOOL=OFF                           \
+    -DCBF_ENABLE_PYTHON:BOOL=OFF                         \
+    -DCBF_WITH_HDF5:BOOL=OFF                             \
+    -DCBF_WITH_LIBTIFF:BOOL=OFF                          \
+    -DCBF_WITH_PCRE2:BOOL=OFF                            \
     "${SRC_DIR}/cbflib"
 
 cmake --build .
